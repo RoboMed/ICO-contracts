5c56a4e1979ef53c6fe573da4f2d5c1f9438746f
RoboMed <i.serikov@robo-med.com>
2017-09-18 15:59:33 +0300

    Add files via upload

---
+++ src/contracts/constants.js
@@ -0,0 +1,181 @@
+string public constant name = "RobomedToken";
+
+  string public constant symbol = "RBM";
+
+  uint256 public constant decimals = 18;
+
+  //VipPlacement constants
+  /**
+   *VipPlacement stage tokens amount
+  */
+  uint256 public constant INITIAL_COINS_FOR_VIPPLACEMENT = 87 937 500 * 10 ** 18;
+
+  /**
+   * VipPlacemant stage Duration
+  */
+  uint256 public constant DURATION_VIPPLACEMENT = 1 seconds;//  1days;
+
+  //end VipPlacement constants
+
+  //PreSale constants
+
+  /**
+   *Presale stage token amount
+  */
+  uint256 public constant EMISSION_FOR_PRESALE = 76 212 500* 10 ** 18;
+
+  /**
+   * PreSale Stage Duration
+  */
+  uint256 public constant DURATION_PRESALE = 1 seconds; // 10 days;
+
+  /**
+   *Presale Stage rate (RBM for 1 ETH) 
+  */
+  uint256 public constant RATE_PRESALE = 2702;
+
+  //end PreSale constants
+
+  //SaleStage1 constants
+
+  /**
+   * ICO stage total Duration
+  */
+  uint256 public constant DURATION_SALESTAGES = 1 hours//30 days;
+
+  /**
+   *Bonus 22% stage rate (actual RBM for 1 ETH) 
+  */
+  uint256 public constant RATE_SALESTAGE1 = 2536;
+
+  /**
+   * Bonus 22% stage token amount
+  */
+  uint256 public constant EMISSION_FOR_SALESTAGE1 = 100835000;
+
+  //end SaleStage1 constants
+
+  //SaleStage2 constants
+
+  /**
+   *Bonus 19%  stage Rate (actual RBM for 1 ETH)
+  */
+  uint256 public constant RATE_SALESTAGE2 = 2473;
+
+  /**
+  * Bonus 19% stage token amount
+  */
+  uint256 public constant EMISSION_FOR_SALESTAGE2 = 100835000;
+
+  //end SaleStage2 constants
+
+  //SaleStage3 constants
+
+  /**
+     *Bonus 15%  stage Rate (actual RBM for 1 ETH)
+  */
+  uint256 public constant RATE_SALESTAGE3 = 2390;
+
+  /**
+  * Bonus 15% stage amount 
+  */
+  uint256 public constant EMISSION_FOR_SALESTAGE3 = 100835000;
+  //end SaleStage3 constants
+
+  //SaleStage4 constants
+
+  /**
+   *Bonus 13% stage rate (actual RBM for 1 ETH)
+  */
+  uint256 public constant RATE_SALESTAGE4 = 2349;
+
+  /**
+   Bonus 13% stage token amount 
+  */
+  uint256 public constant EMISSION_FOR_SALESTAGE4 = 100835000;
+
+  //end SaleStage4 constants
+
+
+  //SaleStage5 constants
+
+  /**
+   * *Bonus 10% stage rate (actual RBM for 1 ETH)
+  */
+  uint256 public constant RATE_SALESTAGE5 = 2286;
+
+  /**
+  *    Bonus 10% stage tokens amount 
+  */
+  uint256 public constant EMISSION_FOR_SALESTAGE5 = 100835000;
+
+  //end SaleStage5 constants
+
+
+
+  //SaleStage6 constants
+
+  /**
+ * *Bonus 7% stage rate (actual RBM for 1 ETH)
+  */
+  uint256 public constant RATE_SALESTAGE6 = 2224;
+
+  /**
+  *Bonus 7% stage tokens amount 
+  */
+  uint256 public constant EMISSION_FOR_SALESTAGE6 = 100835000;
+
+  //end SaleStage6 constants
+
+
+  //SaleStage7 constants
+
+  /**
+   * Курс стадии SaleStage7
+  */
+  uint256 public constant RATE_SALESTAGE7 = 2182;
+
+  /**
+  *** *Bonus 5% stage rate (actual RBM for 1 ETH)
+  */
+  uint256 public constant EMISSION_FOR_SALESTAGE7 = 100835000;
+
+  //end SaleStage7 constants
+
+
+  //SaleStageLast constants
+
+  /**
+ ICO stage duration
+  */
+  uint256 public constant DURATION_SALESTAGELAST = 20 minutes;//30 days;
+
+  /**
+   * Курс стадии SaleStageLast
+  */
+  uint256 public constant RATE_SALESTAGELAST = 2078;
+
+  /**
+  * ICO stage tokens amount
+  */
+  uint256 public constant EMISSION_FOR_SALESTAGELAST = 302505000;
+  //end SaleStageLast constants
+
+  //PostIco constants
+
+  /**
+   * 
+  */
+  uint256 public constant DURATION_NONUSETEAM = 20 minutes;//90 days;
+
+  //end PostIco constants
+
+  /**
+  * Bounty  Tokens amount
+  */
+  uint256 public constant EMISSION_FOR_BOUNTY = 83750000;
+
+  /**
+  * Team  tokens amount
+  */
+  uint256 public constant EMISSION_FOR_TEAM = 418750000;
